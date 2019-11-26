4212cd2b6d30536ca98cd5d4ae211b536576a126
Abdullah Abdelqader <gader177@gmail.com>
2019-11-26 11:30:12 +0200

    Database Migrations

---
+++ database/migrations/2019_11_26_085805_create_categories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('parent_id');
+            $table->string('name');
+            $table->timestamps();
+
+            $table->foreign('parent_id')->references('id')->on('categories')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
